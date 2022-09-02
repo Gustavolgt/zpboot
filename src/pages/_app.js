2abf3affd028271afee8d53eedb5e7c55cafac4d
Gustavolgt <gustavocms16@gmail.com>
2022-09-01 21:41:10 -0300

    pixel

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,7 +9,7 @@ import { ChakraProvider } from '@chakra-ui/react'
 import Script from 'next/script'
 function MyApp({ Component, pageProps }) {
   let GTM_ID = 'GTM-M8NN8M8'
-  const FB_PIXEL_ID = '590741259443714'
+  const FB_PIXEL_ID = '792720518831304'
 
   return (
     <>
