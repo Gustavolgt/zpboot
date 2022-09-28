ddb578ada71c1a86caf4dbbbc100a8a7bbd91493
Gustavolgt <gustavocms16@gmail.com>
2022-09-27 23:27:56 -0300

    app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,7 +9,6 @@ import { ChakraProvider } from '@chakra-ui/react'
 import Script from 'next/script'
 function MyApp({ Component, pageProps }) {
   let GTM_ID = 'GTM-M8NN8M8'
-  const FB_PIXEL_ID = '792720518831304'
 
   return (
     <>
@@ -26,20 +25,6 @@ function MyApp({ Component, pageProps }) {
           `
         }}
       />
-      <Script id="facebook-pixel">
-        {`
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', ${FB_PIXEL_ID});
-        fbq('track', 'ViewContent');
-      `}
-      </Script>
       <ChakraProvider>
         <Component {...pageProps} />
       </ChakraProvider>
